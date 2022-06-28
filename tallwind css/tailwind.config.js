88cbe137ada0174756d2bcfaada6ae47619c965d
ndbao <nguyenducbao166@gmail.com>
2022-06-28 17:03:48 +0700

    set up tallwiind

---
+++ tallwind css/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        barlow : ['"Barlow Condensed"', '"Roboto Condensed"', 'sans-serif'],
+      }
+    },
+  },
+  plugins: [],
+}

00bad815e4e92c4960b60218d120626e7d129583
Nguyễn Đức Bảo <79199646+gechym@users.noreply.github.com>
2022-06-29 16:41:11 +0700

    save component

--- tallwind css/src/App.jsx
+++ tallwind css/src/App.jsx
@@ -4,8 +4,137 @@ function App() {
 
   return (
     <h1 className='text-2xl font-barlow'>Hello world!!</h1>
-      
+    
   )
 }
 
 export default App
+
+
+
+
+// <div class="mx-auto w-full max-w-sm rounded-md border p-4 shadow">
+//   <div class="flex animate-pulse space-x-5">
+//     <!-- <div class="h-10 w-10 rounded-full bg-slate-400"></div> -->
+//     <div class="flex-1 space-y-6 py-1">
+//       <div class="h-36 rounded bg-slate-400"></div>
+//       <div class="space-y-3">
+//         <div class="grid grid-cols-3 gap-4">
+//           <div class="col-span-2 h-2 rounded bg-slate-400"></div>
+//           <div class="col-span-1 h-2 rounded bg-slate-400"></div>
+//         </div>
+//         <div class="grid grid-cols-3 gap-4">
+//           <div class="col-span-2 h-2 rounded bg-slate-400"></div>
+//           <div class="col-span-1 h-2 rounded bg-slate-400"></div>
+//         </div>
+//         <div class="grid grid-cols-1 gap-4">
+//           <div class="col-span-1 h-2 rounded bg-slate-400"></div>
+//         </div>
+//         <div class="grid grid-cols-4 gap-4">
+//           <div class="col-span-2 h-2 rounded bg-slate-400"></div>
+//           <div class="col-span-2 h-2 rounded bg-slate-400"></div>
+//           <div class="col-span-2 h-2 rounded bg-slate-400"></div>
+//           <div class="col-span-1 h-2 rounded bg-slate-400"></div>
+//         </div>
+//         <div class="h-2 rounded bg-slate-400"></div>
+//       </div>
+//     </div>
+//   </div>
+// </div>
+
+// <span class="relative flex h-4 w-4">
+//   <span class="absolute inline-flex h-full w-full animate-ping rounded-full bg-sky-400 opacity-75"></span>
+//   <span class="relative inline-flex h-4 w-4 rounded-full bg-sky-500"></span>
+// </span>
+
+// <button class="mx-2 flex items-center justify-center rounded bg-sky-800 py-2 px-2 text-white shadow-lg shadow-sky-500/50 transition delay-150 duration-300 ease-in-out hover:scale-105 hover:bg-sky-900 focus:outline-none active:opacity-95">
+//   <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+//     <path d="M4 4a2 2 0 012-2h4.586A2 2 0 0112 2.586L15.414 6A2 2 0 0116 7.414V16a2 2 0 01-2 2h-1.528A6 6 0 004 9.528V4z" />
+//     <path fillRule="evenodd" d="M8 10a4 4 0 00-3.446 6.032l-1.261 1.26a1 1 0 101.414 1.415l1.261-1.261A4 4 0 108 10zm-2 4a2 2 0 114 0 2 2 0 01-4 0z" clipRule="evenodd" />
+//   </svg>
+//   Save changes
+// </button>
+
+// <div class="mx-auto max-w-lg overflow-hidden rounded-lg shadow-lg">
+//   <details class="open p-6" open>
+//     <summary class="select-none text-sm font-semibold leading-6 text-slate-900">Why do they call it Ovaltine?</summary>
+//     <div class="mt-3 text-sm leading-6 text-slate-600 dark:text-slate-400">
+//       <p>The mug is round. The jar is round. They should call it Roundtine.</p>
+//     </div>
+//   </details>
+// </div>
+
+// <button class="bg-blue-500 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500">Save Changes</button>
+
+
+
+
+// <div class="
+// mobile:bg-orange-700 
+// laptop:bg-orange-400 
+// tablet:bg-pink-400 
+// mobile:grid-cols-1 
+// mobile:grid-rows-[50px_1fr_50px] 
+// tablet:grid-cols-[1fr_1fr_150px] 
+// tablet:grid-rows-[50px_1fr_1fr] 
+// laptop:grid-cols-[200px_1fr_200px]
+// laptop:grid-rows-[50px_1fr_1fr] 
+// grid min-h-screen gap-2 p-2">
+//   <div class="laptop:col-start-1 laptop:col-end-4 tablet:col-start-1 tablet:col-end-4 bg-gray-500">Header</div>
+//   <div class="laptop:col-start-2 laptop:col-end-2 laptop:row-start-2 laptop:row-end-4 tablet:col-start-1 tablet:col-end-3 tablet:row-start-2 tablet:row-end-4 bg-gray-500">
+//     <div class="grid min-h-screen auto-rows-auto grid-cols-auto gap-2 bg-cyan-100">
+//       <div class="bg-slate-600"></div>
+//       <div class="bg-slate-600"></div>
+//       <div class="bg-slate-600"></div>
+//       <div class="bg-slate-600"></div>
+//       <div class="bg-slate-600"></div>
+//       <div class="bg-slate-600"></div>
+//       <div class="bg-slate-600"></div>
+//       <div class="bg-slate-600"></div>
+//       <div class="bg-slate-600"></div>
+//     </div>
+//   </div>
+//   <div class="laptop:col-start-1 laptop:col-end-1 laptop:row-start-2 laptop:row-end-4 tablet:row-start-2 tablet:row-end-4 bg-gray-500"></div>
+//   <div class="laptop:col-start-3 laptop:col-end-3 laptop:row-start-2 laptop:row-end-4 tablet:row-start-2 tablet:row-end-4 bg-gray-500"></div>
+// </div>
+
+// <!-- <div class="grid min-h-screen grid-cols-[250px_1fr_3fr] grid-rows-[60px_1fr_80px] gap-2 bg-gray-400">
+//   <div class="col-start-1 col-end-4 bg-blue-100">Header</div>
+//   <div class="bg-blue-100">siderbar</div>
+//   <div class="col-start-2 col-end-4 bg-blue-100">
+//     <div class="grid min-h-screen auto-rows-auto grid-cols-auto gap-2 bg-cyan-100">
+//       <div class="bg-slate-600"></div>
+//       <div class="bg-slate-600"></div>
+//       <div class="bg-slate-600"></div>
+//       <div class="bg-slate-600"></div>
+//       <div class="bg-slate-600"></div>
+//       <div class="bg-slate-600"></div>
+//       <div class="bg-slate-600"></div>
+//       <div class="bg-slate-600"></div>
+//       <div class="bg-slate-600"></div>
+//     </div>
+//   </div>
+//   <div class="col-start-1 col-end-4 bg-blue-100"></div>
+// </div> -->
+
+// module.exports = {
+//   theme: {
+//     screens: {
+//       mobile: '320px',
+//       tablet: '640px',
+//       laptop: '1024px',
+//       desktop: '1280px',
+//     },
+
+//     extend: {
+//       gridTemplateColumns: {
+//         auto: 'repeat(auto-fit, minmax(250px, 1fr))',
+//       },
+
+//       gridAutoRows: {
+//         auto: 'minmax(300px, 300px)',
+//       },
+//     },
+//   },
+//   plugins: [],
+// }
